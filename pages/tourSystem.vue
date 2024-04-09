216dceea4c1250f28b1606aff02e2332609902d6
book <a.s.myrzabaev@gmail.com>
2024-04-09 04:53:33 +0300

    update tour page

--- pages/tourSystem.vue
+++ pages/tourSystem.vue
@@ -141,23 +141,29 @@
               <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 13.5H6.5C5.10444 13.5 4.40665 13.5 3.83886 13.6722C2.56045 14.06 1.56004 15.0605 1.17224 16.3389C1 16.9067 1 17.6044 1 19M18 19V13M15 16H21M13.5 5.5C13.5 7.98528 11.4853 10 9 10C6.51472 10 4.5 7.98528 4.5 5.5C4.5 3.01472 6.51472 1 9 1C11.4853 1 13.5 3.01472 13.5 5.5Z" stroke="#526477" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
-
               <p>Запись поциента</p>
             </div>
           </template>
           <div>
-            <h4 class="tabview-custom__caption">
-              “Ыраазычылык билдирем”
-            </h4>
-            <div class="star-group">
-              <span class="star" />
-              <span class="star" />
-              <span class="star" />
-              <span class="star" />
-              <span class="star" />
-            </div>
-            <p class="tabview-custom__description">
-              Бул сайтты ишке ашыргандарга ыраазылык билдирем. Аябай жакшы программа болуптур. Стоматологдордун кыйналып кылган иштерин жеңилдетет экен. Excel деген нерседен кутулуп жакшы болбодубу. Каалаган форматта отчётторду көчүрүп алсаң болот экен, пациенттерге кабар таратуу бизде аябай кыйын иш эле, бул жакта бир бассаң эле болуп калат экен, тушунуктуу, кыскача, ыңгайлуу, аябай сонун болуптур. Башка клиникаларга сунуштайт элем.16:32
+            <p class="tour-system__description">
+              Чтобы записать пациента на прием, перейдите в раздел «Календарь». Перед вами появится график работы доктора на сегодняшний день.
+            </p>
+            <img src="/layout/images/image%2025(9).png" alt="" class="tour-system__img">
+            <p class="tour-system__description">
+              Затем заполняете карточку (обязательные поля обозначены *). Пример заполнения карточки доктора показано на рисунках ниже:
+            </p>
+            <img src="/layout/images/image%2025(10).png" alt="" class="tour-system__img">
+            <img src="/layout/images/image%2025(11).png" alt="" class="tour-system__img">
+            <p class="tour-system__description">
+              Затем заполняете карточку (обязательные поля обозначены *). Пример заполнения карточки доктора показано на рисунках ниже:
+            </p>
+            <img src="/layout/images/image%2025(12).png" alt="" class="tour-system__img">
+            <p class="tour-system__description">
+              Затем заполняете карточку (обязательные поля обозначены *). Пример заполнения карточки доктора показано на рисунках ниже:
+            </p>
+            <img src="/layout/images/image%2025(13).png" alt="" class="tour-system__img">
+            <p class="tour-system__description">
+              Затем заполняете карточку (обязательные поля обозначены *). Пример заполнения карточки доктора показано на рисунках ниже:
             </p>
           </div>
         </TabPanel>
